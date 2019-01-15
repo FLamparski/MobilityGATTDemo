e9ce355b16f2fe4613a5510d5c682383bba9a109
Filip Wieland <filiplamparski@gmail.com>
2019-01-15 08:49:29 +0000

    Attempt to time-sync

--- MobilityGATTDemo.ino
+++ MobilityGATTDemo.ino
@@ -1,4 +1,5 @@
 #include <Arduino.h>
+#include <ArduinoJson.h>
 #include <freertos/FreeRTOS.h>
 #include <freertos/event_groups.h>
 #include <sstream>
@@ -12,6 +13,11 @@
 EventGroupHandle_t eventFlags = xEventGroupCreate();
 
 AppBLEService bleService;
+StaticJsonBuffer<128> JSONBuffer;
+// Terrible, no good way of doing clock synchronization
+long syncTimestamp = 0;
+// Would you think of thread safety!
+bool hasSync = false;
 
 void setup() {
     Serial.begin(115200);
@@ -34,13 +40,26 @@ void setup() {
         if (rxValue.find("APP_GET_SAMPLES") != std::string::npos) {
             xEventGroupSetBits(eventFlags, EVT_CMD_GET_SAMPLES);
         }
+        if (rxValue.find("APP_ALIVE") != std::string::npos) {
+            const char *buf = rxValue.c_str();
+            JsonObject& msg = JSONBuffer.parseObject(buf);
+            if (!msg.success()) {
+                Serial.println("APP_ALIVE parse failed");
+            }
+            else {
+                int ts = msg["ts"];
+                syncTimestamp = ts;
+                hasSync = true;
+            }
+            JSONBuffer.clear();
+        }
     });
     bleService.start("Mobility Mk 1");
 
     xTaskCreate(taskConnectionHandler, "connection-handler", 3 * 1024, nullptr, tskIDLE_PRIORITY, nullptr);
 }
 
-// Currently not used
+// TODO: Output message queueing
 void transmitKeepalive(int timestamp) {
     std::ostringstream buffer;
     buffer << "{\"type\":\"DEVICE_ALIVE\",\"ts\":" << timestamp << "}\r\n";
@@ -52,7 +71,8 @@ void transmitSamples(int timestamp) {
     std::ostringstream buffer;
     buffer << "{\"type\":\"DEVICE_SAMPLES\",\"ts\":" << timestamp << "\",\"payload\":[";
     for (int i = 10; i >= 0; i--) {
-        buffer << "{\"roundId\":" << i << ",\"device\":\"11:22:33:44:55:66\",\"type\":\"MOBILITY_DATA\",\"distanceDriven\":230.54}";
+        int roundId = timestamp - timestamp % 60 - i * 60;
+        buffer << "{\"roundId\":" << roundId << ",\"device\":\"11:22:33:44:55:66\",\"type\":\"MOVEMENT_ROUND_DATA\",\"distanceDriven\":230.54}";
         if (i > 0) {
             buffer << ",";
         }
@@ -77,6 +97,11 @@ void taskConnectionHandler(void *params) {
             pdFALSE,
             100
         );
+        if (hasSync && abs(syncTimestamp - timestampTimer) >= 2) {
+            Serial.printf("Setting timestamp (currently %d) to %d\n", timestampTimer, syncTimestamp);
+            timestampTimer = syncTimestamp;
+            hasSync = false;
+        }
         switch (state) {
             case NOT_CONNECTED:
                 if (evt & EVT_FLAG_CONNECTED) {
@@ -94,6 +119,10 @@ void taskConnectionHandler(void *params) {
                     if (keepaliveTimer == 0) {
                         keepaliveTimer = KEEPALIVE_PERIOD;
                         timestampTimer++;
+                        if (timestampTimer % 30 == 0) {
+                            Serial.printf("syncTimestamp is %d\n", syncTimestamp);
+                            transmitKeepalive(timestampTimer);
+                        }
                     }
                     if (evt & EVT_CMD_GET_SAMPLES) {
                         transmitSamples(timestampTimer);
